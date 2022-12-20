a4ff258bb8e03dfd00a68d125676118bc6b02e24
Olesya <o.verstak99@gmail.com>
2022-12-20 21:16:24 +0700

    41 and 42 task done

---
+++ task41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите элементы(через пробел): ");
+
+int[] MyArr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+System.Console.WriteLine(FindNumber(MyArr));
+
+string FindNumber(int[] arr)
+{
+    int count = 0;
+   for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            count ++;
+    }
+    return $"Количество чисел больше ноля: {count}";
+}
+
---
+++ task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+int b1 = inputNumber("Введите b1");
+int b2 = inputNumber("Введите b1");
+int k1 = inputNumber("Введите k1");
+int k2 = inputNumber("Введите k2");
+
+double x = (b2 - b1)/(k1-k2);
+double y = k1 * x + b1;
+Console.WriteLine ($"Точка пересечения двух прямых = [{x};{y}]");
+
+int inputNumber(string str)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        System.Console.WriteLine(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+
